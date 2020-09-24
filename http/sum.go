5eeef498d82e666abbfea264b3589359a020ee02
liushepeng <liushepeng@sohu.com>
2020-09-24 14:42:18 +0800

    sn

--- http/sum.go
+++ http/sum.go
@@ -27,6 +27,12 @@ var TotalCache = struct {
 }{LastTimes: new(int64)}
 
 func TotalHandle(w http.ResponseWriter, req *http.Request) {
+	b := checkRoutine()
+	defer atomic.AddInt32(RoutineConter, -1)
+	if !b {
+		WriteErr(w, "HTTP_ROUTINE:Too many routines")
+		return
+	}
 	if !checkIp(req.RemoteAddr) {
 		WriteErr(w, fmt.Sprintf("Invalid IP:%s", req.RemoteAddr))
 		return
