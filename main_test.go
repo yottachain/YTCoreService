8fe732e05816416eab61c24430830f3fe73c2a86
liushepeng <liushepeng@sohu.com>
2022-12-05 15:23:33 +0800

    ..

--- main_test.go
+++ main_test.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/mr-tron/base58/base58"
 	"github.com/yottachain/YTCoreService/examples"
+	"github.com/yottachain/YTCrypto"
 )
 
 func Test_Test(t *testing.T) {
-	examples.TestCodec()
+	s, _ := YTCrypto.Sign("5JnLRW1bTRD2bxo93wZ1qnpXfMDHzA97qcQjabnoqgmJTt7kBoH", []byte("lsptest"))
+
+	fmt.Println(s)
+	pub, _ := YTCrypto.GetPublicKeyByPrivateKey("5JnLRW1bTRD2bxo93wZ1qnpXfMDHzA97qcQjabnoqgmJTt7kBoH")
+
+	bs, _ := YTCrypto.ECCEncrypt([]byte("lsptest"), pub)
+	fmt.Println(base58.Encode(bs))
 }
 
 func Test_S3(t *testing.T) {
@@ -22,6 +31,6 @@ func Test_SetVersion(t *testing.T) {
 	SetVersion()
 }
 
-func coder() {
-
+func Test_Codec(t *testing.T) {
+	examples.TestCodec()
 }
